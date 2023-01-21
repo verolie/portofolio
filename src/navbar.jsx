4fb2354d77197c5817b1740b81444f6e8369e8d2
VrowLie <veyolie@gmail.com>
2023-01-21 18:14:39 +0700

    menu bar update
    continue adding new fitur at navigation bar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import './component/navbar.scss'
+import React from "react";
+import "./component/navbar.scss";
+import { Icon } from '@iconify/react';
 
 function Navbar() {
-    return (
+  return (
     <header>
-        <div className="Navbar">
-            <div className="NavLog">
-                <h1 className='NavHello'>Hello,</h1>
-                <h1 className='NavName'>my name Vero</h1>
-            </div>
-            <nav className="NavRoutes">
-                <a href="/#">Home</a>
-                <a href="/#">About</a>
-                <a href="/#">Experience</a>
-                <a href="/#">Certificate</a>
-                <a href="/#">Skills</a>
-            </nav>
+      <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
+
+      <div className="Navbar">
+        <div className="NavLog">
+          <h1 className="NavHello">Hello,</h1>
+          <h1 className="NavName">my name Vero</h1>
         </div>
+        <nav className="NavRoutes">
+          <a href="/#" className="active">
+            Home
+          </a>
+          <a href="/#">About</a>
+          <a href="/#">Experience</a>
+          <a href="/#">Certificate</a>
+          <a href="/#">Skills</a>
+        </nav>
+        <Icon icon="bx:menu" id="menu-icon"/>
+      </div>
     </header>
-    )
-  }
-  
-  export default Navbar
+  );
+}
+
+let menu = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".NavRoutes")
+
+menu.onclick = () => {
+    menu.classList.toggle("bx:x")
+    navbar.classList.toggle("open");
+}
+
+export default Navbar;
