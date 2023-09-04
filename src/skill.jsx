b5ac332cc5022fb9ec694ae7f7089616328b0454
Veronica <Veronica@finexusgroup.com>
2023-09-04 13:56:32 +0700

    add skill

---
+++ src/skill.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./component/skill.scss";
+
+
+function Skill() {
+    return (
+       <div className="skill">
+            <div className="block">
+                <div className="title">Skill</div>
+            </div>
+        </div>
+    );
+}
+
+export default Skill;
