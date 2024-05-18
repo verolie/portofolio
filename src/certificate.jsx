c7ebe8c4e58ed9b2545819825873d5382b356e3c
verolie <veyolie@gmail.com>
2024-05-18 19:51:50 +0700

    add new style

--- src/certificate.jsx
+++ src/certificate.jsx
@@ -20,9 +20,9 @@ function Certificate() {
                             <p className="certificate-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut porta quam. Aliquam mauris nibh, euismod eget ante porta, iaculis scelerisque quam. Cras lacinia bibendum tristique. Aliquam erat volutpat. Aenean ac ultricies quam, in lacinia libero. Mauris sollicitudin aliquet magna in vulputate. Vivamus a enim vitae tortor lacinia consequat. Cras condimentum diam sit amet tempus lacinia. Vestibulum laoreet fringilla mauris vel tincidunt. Sed venenatis, sem in venenatis egestas, lacus justo luctus ipsum, a fringilla ex sem ac neque. Morbi efficitur diam sit amet pretium semper. Cras vel tellus nec nisi lacinia placerat.</p> 
                         </div>
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <div className="block">
-                            <p className="button">Gajiku</p> 
+                            <p className="text-date">Oct 2022</p> 
                         </div>
                     </div>
                 </div>
